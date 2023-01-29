24d6a1d54d95fe2f78deadc7ddef9b4ea31f4a82
Andrey Barabolya <andrey_ba@tut.by>
2023-01-29 23:42:46 +0300

    feat: create programm

--- src/index.js
+++ src/index.js
@@ -37,8 +37,37 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
+function decode(mass) {
     // write your solution here
+    let newArray = [];
+    let morseStr = '';
+    let result = '';
+    for (let i = 0; i < mass.length; i += 10) {
+        newArray.push(mass.slice(i, i + 10));
+    }
+    for (let value of newArray) {
+        //console.log(value);
+        morseStr = '';
+        for (let i = 0; i < value.length; i += 2) {
+            if(value.slice(i, i + 2) == 10){
+                morseStr += '.';
+            }
+            if(value.slice(i, i + 2) == 11){
+                morseStr += '-';
+            }
+            if(value.slice(i, i + 2) == '**'){
+                morseStr += '%';
+            }
+        }
+        if (morseStr == '%%%%%'){
+            result += ' ';
+        }
+        else {
+            result += MORSE_TABLE[morseStr];
+        }
+    }
+    return result;
+
 }
 
 module.exports = {
